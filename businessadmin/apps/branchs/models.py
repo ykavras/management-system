ec0f8d107a8f0d2f70b2d0067856f6acc49e9ed8
ishakoktn@gmail.com <ishak.okutan@orgino.com.tr>
2019-06-05 03:17:40 +0300

    add str methods

--- businessadmin/apps/branchs/models.py
+++ businessadmin/apps/branchs/models.py
@@ -8,11 +8,30 @@ class Department(models.Model):
         verbose_name = 'Bölüm'
         verbose_name_plural = 'Bölümler'
 
+    def __str__(self):
+        return self.name
+
 
 class Branch(models.Model):
     department = models.ForeignKey(Department, models.PROTECT, related_name='branchs')
     name = models.CharField(verbose_name='Adı', max_length=255)
+    short_name = models.CharField(verbose_name='Kısaltılmışi', max_length=10)
 
     class Meta:
         verbose_name = 'Dal'
         verbose_name_plural = 'Dallar'
+
+    def __str__(self):
+        return self.short_name
+
+
+class Klass(models.Model):
+    name = models.CharField(verbose_name='Sınıf', max_length=10, help_text='Örneğin: "12"')
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='branches', verbose_name='Dal')
+
+    class Meta:
+        verbose_name = 'Sınıf'
+        verbose_name_plural = 'Sınıflar'
+
+    def __str__(self):
+        return f'{self.name} {self.branch.short_name}'
